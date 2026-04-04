466261175333dc64b1981d6954a16e6bcf49fb15
jeremie1207 <jbarh086@uottawa.ca>
2026-04-03 21:51:46 -0400

    create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+import logo from "../assets/quiz-logo.png";
+
+export default function Header() {
+    return <header>
+        <img src={logo} alt="" />
+        <h1>React quiz</h1>
+    </header>
+}
